97910efca552d2a1bdd39f7874677df1b5c8bac8
bray-hiramis <johnbrianhiramis@gmail.com>
2025-10-15 20:25:33 +0800

    added the display trasaction feature

--- js/script.js
+++ js/script.js
@@ -11,8 +11,10 @@ const txtAmount = document.getElementById('amount');
 // button
 const btnAdd = document.getElementById('btn-add');
 
-
+// array to store the stringify object of the transaction (description, amount)
 let transactions = [];
+
+// This handles the logic of getting the items in the local storage
 const storedTransactions = localStorage.getItem('transactions');
 if (storedTransactions !== null) {
    transactions = JSON.parse(storedTransactions);
@@ -23,11 +25,13 @@ if (storedTransactions !== null) {
 // Adding new transactions
 const form = document.getElementById('form');
 
+// function to update the local storage for new transaction
 function updateLocalStorage() {
    const jsonString = JSON.stringify(transactions);
    localStorage.setItem('transactions', jsonString)
 }
 
+// function to add the transaction to the local storage
 function addTransaction(e) {
    e.preventDefault();
 
@@ -52,8 +56,44 @@ function addTransaction(e) {
    txtDescription.value = '';
    txtAmount.value = '';
    updateLocalStorage();
+   displayTransactions();
 }
 
+// console.log(transactions);
+
+// This is the event listener inside the Form section in HTML
+form.addEventListener('submit', addTransaction);
+
+// ====================================================================
+
+// Implement displayTransactions() function
 console.log(transactions);
 
-form.addEventListener('submit', addTransaction);
+function createTransactionElement(transaction) {
+   const li = document.createElement('li');
+   const deleteBtn = document.createElement('button')
+
+
+   const itemClass = transaction.amount < 0 ? 'minus' : 'plus';
+   li.classList.add(itemClass);
+
+   const sign = transaction.amount < 0 ? '-': '+';
+
+   deleteBtn.classList.add('delete-btn');
+   deleteBtn.setAttribute('data-id', transaction.id);
+   deleteBtn.innerText = 'X';
+
+   li.innerHTML = `${transaction.description}<span>${sign}${Math.abs(transaction.amount).toFixed(2)}</span>${deleteBtn.outerHTML}`;
+
+   return li;
+}
+
+function displayTransactions() {
+   historyList.innerHTML = '';
+
+   transactions.forEach(function(transaction) {
+      const transactionElement = createTransactionElement(transaction);
+      historyList.append(transactionElement);
+   })
+}
+displayTransactions();
