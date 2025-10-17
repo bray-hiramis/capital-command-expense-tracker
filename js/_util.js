108734d2af8ab2359b93b0b6e0f5620e143d2cf6
bray-hiramis <johnbrianhiramis@gmail.com>
2025-10-17 20:00:31 +0800

    Active nav feature

---
+++ js/_util.js
@@ -0,0 +1,21 @@
+const navList = document.querySelectorAll('[data-nav="nav"]');
+
+console.log(navList);
+
+function handleTabClick(e) {
+   e.preventDefault();
+
+   navList.forEach(link => {
+      link.classList.remove('current-tab');
+   });
+
+   this.classList.add('current-tab');
+}
+
+navList.forEach(link => {
+   link.addEventListener('click', handleTabClick);
+})
+
+if (navList.length > 0) {
+   navList[0].classList.add('current-tab');
+}
